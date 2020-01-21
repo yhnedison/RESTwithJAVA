9ea9af51f4d7557f0a7a5dc7cded2326c39fe42b
hyu <hyu@imedris.com>
2020-01-20 16:31:38 -0800

    update

--- src/main/java/com/alexyu/messenger/resources/MessageResource.java
+++ src/main/java/com/alexyu/messenger/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,9 +16,19 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public List<Message> getMessage() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
+		
 		return messageService.getAllMessages();
 		// Jersey is responsble for converting models to xml here
 	}
+	
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
+	}
+	
 }
--- src/main/java/com/alexyu/messenger/service/MessageService.java
+++ src/main/java/com/alexyu/messenger/service/MessageService.java
@@ -2,22 +2,43 @@ package com.alexyu.messenger.service;
 
 import java.util.*;
 
+import com.alexyu.messenger.database.DatabaseClass;
 import com.alexyu.messenger.model.Message;
 
 public class MessageService {
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "Alex Yu"));
+		messages.put(2L, new Message(1, "Hello World2", "Alex Yu2"));
+		messages.put(3L, new Message(1, "Hello World3", "Alex Yu3"));
+		messages.put(4L, new Message(1, "Hello World4", "Alex Yu4"));
+	}
 	// mimic calling DB and get data
 	public List<Message> getAllMessages() {
-		Message m1 = new Message(1, "Hello World", "Alex Yu");
-		Message m2 = new Message(1, "Hello World2", "Alex Yu2");
-		Message m3 = new Message(1, "Hello World3", "Alex Yu3");
-		Message m4 = new Message(1, "Hello World4", "Alex Yu4");
-		List<Message> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		list.add(m3);
-		list.add(m4);
-		
-		return list;
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 }
